6d913787ce86aba8b26d517f939d7541cf5bccae
amoschoomy <acho0057@student.monash.edu>
2021-08-05 21:23:53 +0800

    Completed name input and name output function

--- diem-js-challenge-master/handler.js
+++ diem-js-challenge-master/handler.js
@@ -1,1 +1,12 @@
-let nameInput=document.getElementById("name")
+const nameInput=document.getElementById("name")
+const nameOutput=document.getElementById("nameOutput")
+const textInput="Hello there! What's your name?"
+
+
+nameInput.onfocus=()=>{
+    nameOutput.textContent=textInput
+}
+
+nameInput.onblur=()=>{
+    nameOutput.textContent=nameInput.value?"Hi, "+nameInput.value:textInput
+}
