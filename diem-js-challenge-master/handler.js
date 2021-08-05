4de431f7c4a5576f245ad79f14e9c5cf5b1e6cd6
amoschoomy <acho0057@student.monash.edu>
2021-08-05 22:58:29 +0800

    Done birthday function

--- diem-js-challenge-master/handler.js
+++ diem-js-challenge-master/handler.js
@@ -2,11 +2,25 @@ const nameInput=document.getElementById("name")
 const nameOutput=document.getElementById("nameOutput")
 const textInput="Hello there! What's your name?"
 
-
 nameInput.onfocus=()=>{
     nameOutput.textContent=textInput
 }
 
 nameInput.onblur=()=>{
     nameOutput.textContent=nameInput.value?"Hi, "+nameInput.value:textInput
+}
+
+const birthdaySelector=document.getElementById("date")
+const ageOutput=document.getElementById("ageOutput")
+const birthdayText="Lemme guess, your age is..."
+
+birthdaySelector.onfocus=()=>{
+    ageOutput.textContent=birthdayText
+}
+
+birthdaySelector.onblur=()=>{
+    const date=new Date(birthdaySelector.value)
+    const year=new Date().getFullYear()-date.getFullYear()
+    ageOutput.textContent=birthdaySelector.value?"Your age is "+year:"Would you mind entering your birthday?"
+
 }
