3970853534fd2bb5f6697c1afbed781373c5225f
amoschoomy <acho0057@student.monash.edu>
2021-08-06 21:33:39 +0800

    Finished theme task

--- diem-js-challenge-master/handler.js
+++ diem-js-challenge-master/handler.js
@@ -24,3 +24,20 @@ birthdaySelector.onblur=()=>{
     ageOutput.textContent=birthdaySelector.value?"Your age is "+year:"Would you mind entering your birthday?"
 
 }
+
+const lightSelect=document.getElementById("light")
+const darkSelect=document.getElementById("dark")
+const rightSection=document.getElementById("right-section")
+const themeOutput=document.getElementById("themeOutput")
+
+lightSelect.onchange=()=>{
+    rightSection.classList.remove("bg-dark","text-white")
+    rightSection.classList.add("bg-light","text-dark")
+    themeOutput.textContent="You selected Light Mode!"
+}
+
+darkSelect.onchange=()=>{
+    rightSection.classList.remove("bg-light","text-dark")
+    rightSection.classList.add("bg-dark","text-white")
+    themeOutput.textContent="You selected Dark Mode!"
+}
